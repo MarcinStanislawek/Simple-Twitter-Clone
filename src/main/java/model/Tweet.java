1dedfb023d3ea3cd0b0171ff0a80e0446afb9b16
Marcin Stanisławek <mstanislawek@gmail.com>
2019-10-06 09:45:56 +0200

    #6 Implemented tweet model.

--- src/main/java/model/Tweet.java
+++ src/main/java/model/Tweet.java
@@ -1,12 +1,59 @@
 package model;
 
+import javax.persistence.*;
 import java.util.Date;
 
+@Entity
+@Table(name = "tweets")
 public class Tweet {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private long id;
+
+    @ManyToOne(cascade = {CascadeType.MERGE, CascadeType.PERSIST})
+    @JoinColumn(name = "id_author")
     private User author;
+
+    @Column(name= "publish_date")
     private Date publishDate;
+
+    @Column(name = "message")
     private String message;
 
+    public Tweet() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public User getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(User author) {
+        this.author = author;
+    }
+
+    public Date getPublishDate() {
+        return publishDate;
+    }
+
+    public void setPublishDate(Date publishDate) {
+        this.publishDate = publishDate;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
